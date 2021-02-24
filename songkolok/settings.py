b22a3d5a4ebff3a3e97cdea13154b0f55dfdad90
Abdunnoor11 <abdunnoor11@gmail.com>
2021-02-24 16:15:49 +0600

    heroku add

--- songkolok/settings.py
+++ songkolok/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import django_heroku
 from pathlib import Path
 import os
 
@@ -128,3 +128,4 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
+django_heroku.settings(locals())
